abb92d200a1b3976952a794c4808471bf20b73c3
Said <said.badalov@saytqur.az>
2025-11-09 17:28:46 +0400

    chore: redux store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
-const App = () => <></>;
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <></>
+    </Provider>
+  );
+};
 
 export default App;
